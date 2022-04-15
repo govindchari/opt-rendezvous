39bb63ea1c0563f3220c62c6351a7596df325555
Govind Chari <govindchari1@gmail.com>
2022-04-14 20:17:55 -0400

    Added l1 norm

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import cvxpy as cp
 import numpy as np
-from scipy import sparse
 from matplotlib import pyplot as plt
 import utils
 
-n = 0.00113
+# Prediction horizon
 dt = 1
+time = 3000
+N = int(time/dt)
+
+#Physics parameters
+n = 0.00113
 Ad, Bd = utils.getABd_zoh(n,dt)
 [nx, nu] = Bd.shape
 
-# Objective function
-R = sparse.eye(3)
-
-# Initial and reference states
+# Initial and terminal conditions
 x0 = np.array([0.,100.,0.,0.,0.,0.])
 xT = np.array([0.,0.,0.,0.,0.,0.])
-
-# Prediction horizon
-N = 3000
 
 # Define problem
 u = cp.Variable((nu, N))
@@ -28,14 +26,15 @@ constraints = [x[:,0] == x0]
 constraints += [x[:,N-1] == xT]
 
 for k in range(N):
-    #objective += cp.norm(u[:,k], 1)
-    objective += cp.sum_squares(u[:,k])
+    #objective += cp.norm(u[:,k], 1)        #L1-norm penalty
+    objective += cp.sum_squares(u[:,k])    #L2-norm penalty
     constraints += [x[:,k+1] == Ad@x[:,k] + Bd@u[:,k]]
 
 prob = cp.Problem(cp.Minimize(objective), constraints)
 
 prob.solve()
 
+#Plotting
 plt.figure()
 plt.plot(x.value[1,:],x.value[0,:])
 
