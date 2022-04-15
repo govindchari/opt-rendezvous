feea3e61056256cde0e4154286af78cc7f3294b4
Govind Chari <govindchari1@gmail.com>
2022-04-14 20:32:01 -0400

    Added input constraint

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utils
 
 # Prediction horizon
 dt = 1
-time = 3000
+time = 250
 N = int(time/dt)
 
 #Physics parameters
@@ -14,25 +14,27 @@ Ad, Bd = utils.getABd_zoh(n,dt)
 [nx, nu] = Bd.shape
 
 # Initial and terminal conditions
-x0 = np.array([0.,100.,0.,0.,0.,0.])
+x0 = np.array([0.,-100.,0.,0.,0.,0.])
 xT = np.array([0.,0.,0.,0.,0.,0.])
 
 # Define problem
 u = cp.Variable((nu, N))
 x = cp.Variable((nx, N+1))
+umax = 0.01
 x_init = cp.Parameter(nx)
 objective = 0
 constraints = [x[:,0] == x0]
 constraints += [x[:,N-1] == xT]
 
 for k in range(N):
-    #objective += cp.norm(u[:,k], 1)        #L1-norm penalty
-    objective += cp.sum_squares(u[:,k])    #L2-norm penalty
+    objective += cp.norm(u[:,k], 1)        #L1-norm penalty
+    #objective += cp.sum_squares(u[:,k])    #L2-norm penalty
     constraints += [x[:,k+1] == Ad@x[:,k] + Bd@u[:,k]]
+    constraints += [cp.norm(u[:,k]) <= umax]
 
 prob = cp.Problem(cp.Minimize(objective), constraints)
 
-prob.solve()
+prob.solve(verbose = True)
 
 #Plotting
 plt.figure()
