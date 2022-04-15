739ae00fb1fa7f738c513353c1203c73779bee6b
Govind Chari <govindchari1@gmail.com>
2022-04-14 20:09:54 -0400

    Working unconstrained optimization

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-import numpy as np
 import cvxpy as cp
+import numpy as np
+from scipy import sparse
+from matplotlib import pyplot as plt
 import utils
 
-# Problem data
-Ac, Bc = (utils.getABc(1))
-Ad, Bd = (utils.getABd(1,1))
+n = 0.00113
+dt = 1
+Ad, Bd = utils.getABd_zoh(n,dt)
+[nx, nu] = Bd.shape
+
+# Objective function
+R = sparse.eye(3)
+
+# Initial and reference states
+x0 = np.array([0.,100.,0.,0.,0.,0.])
+xT = np.array([0.,0.,0.,0.,0.,0.])
+
+# Prediction horizon
+N = 3000
+
+# Define problem
+u = cp.Variable((nu, N))
+x = cp.Variable((nx, N+1))
+x_init = cp.Parameter(nx)
+objective = 0
+constraints = [x[:,0] == x0]
+constraints += [x[:,N-1] == xT]
+
+for k in range(N):
+    #objective += cp.norm(u[:,k], 1)
+    objective += cp.sum_squares(u[:,k])
+    constraints += [x[:,k+1] == Ad@x[:,k] + Bd@u[:,k]]
+
+prob = cp.Problem(cp.Minimize(objective), constraints)
+
+prob.solve()
+
+plt.figure()
+plt.plot(x.value[1,:],x.value[0,:])
+
+plt.figure()
+plt.plot(np.transpose(u.value))
+
+plt.show()
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def getABd(n, dt):
     nt = n*dt
     snt = np.sin(nt)
     cnt = np.cos(nt)
-    Ad = np.array([[      4.0 - 3.0 * cnt, 0.0,      0.0,         (1.0 / n) * snt,              (2.0 / n) * (1 - cnt),             0.0],
+    Ad = np.array([[    4.0 - 3.0 * cnt, 0.0,      0.0,         (1.0 / n) * snt,              (2.0 / n) * (1 - cnt),             0.0],
                  [     6.0 * (snt - nt), 1.0,      0.0, (2.0 / n) * (cnt - 1.0), (1.0 / n) * (4.0 * snt - 3.0 * nt),             0.0],
                  [                  0.0, 0.0,      cnt,                     0.0,                                0.0, (1.0 / n) * snt],
                  [        3.0 * n * snt, 0.0,      0.0,                     cnt,                          2.0 * snt,             0.0],
@@ -18,6 +18,27 @@ def getABd(n, dt):
                   [                    (1.0 / n) * snt,                                 -(2.0 / n) * cnt,                  dt],
                   [                    (2.0 / n) * cnt,                       (4.0 / n) * snt - 3.0 * dt,                  dt],
                   [                                 dt,                                               dt,     (1.0 / n) * snt]
+                ])
+    return (Ad, Bd)
+
+def getABd_zoh(n, dt):
+    nt = n*dt
+    snt = np.sin(nt)
+    cnt = np.cos(nt)
+    Ad = np.array([[    4.0 - 3.0 * cnt, 0.0,      0.0,         (1.0 / n) * snt,              (2.0 / n) * (1 - cnt),             0.0],
+                 [     6.0 * (snt - nt), 1.0,      0.0, (2.0 / n) * (cnt - 1.0), (1.0 / n) * (4.0 * snt - 3.0 * nt),             0.0],
+                 [                  0.0, 0.0,      cnt,                     0.0,                                0.0, (1.0 / n) * snt],
+                 [        3.0 * n * snt, 0.0,      0.0,                     cnt,                          2.0 * snt,             0.0],
+                 [6.0 * n * (cnt - 1.0), 0.0,      0.0,              -2.0 * snt,                    4.0 * cnt - 3.0,             0.0],
+                 [                  0.0, 0.0, -n * snt,                     0.0,                                0.0,             cnt]
+                ])
+
+    Bd = np.array([[0.5 * dt**2,         0.0,         0.0],
+                   [        0.0, 0.5 * dt**2,         0.0],
+                   [        0.0,         0.0, 0.5 * dt**2],
+                   [         dt,         0.0,         0.0],
+                   [        0.0,          dt,         0.0],
+                   [        0.0,         0.0,          dt],
                 ])
     return (Ad, Bd)
 
